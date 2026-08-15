c2b848f023608aed8b23e26b7fc3359f287e0db7
Victtor-vilarim <1.jVicttor.1@gmail.com>
2026-08-15 14:48:55 -0300

    Fix: .gitignore

--- main/__init__.py
+++ main/__init__.py
@@ -5,6 +5,6 @@ main_bp = Blueprint('main', __name__, template_folder='templates')
 main_bp.add_url_rule('/download_file/<string:name>', endpoint='download_file', build_only=True)
 main_bp.add_url_rule('/download_img/<string:img_name>', endpoint='download_img', build_only=True)
 
-UPLOAD_FOLDER = Path(main_bp.root_path) / 'tmp'
+UPLOAD_FOLDER = Path(main_bp.root_path) / 'media'
 
 from . import routes
